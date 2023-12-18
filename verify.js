b28ff559e9771cde539e39b3a03fa52adc26f612
Santhosh <shanmugamsanthosh22@gmail.com>
2023-12-18 12:15:57 +0530

    today--

--- verify.js
+++ verify.js
@@ -1,21 +1,33 @@
-var admin = require("./configuration.js");
+// var admin = require("./configuration.js");
 
 var decodeToken = function(req, res, next) {
-  var {token} = req.headers;
-  try {
-    admin.auth().verifyIdToken(token)
-      .then(function(decodedToken) {
-        req.userdetails = decodedToken;
-        next();
-      })
-      .catch(function(error) {
-        console.error("Error verifying ID token:", error);
-        res.status(401).json("Invalid Access Token May be Expired");
-      });
-  } catch (error) {
-    console.error("Error verifying ID token:", error);
-    res.status(401).json("Invalid Access Token May be Expired");
+  var {email} = req.body;
+  var {secret} = req.headers;
+
+  if(secret === process.env.CLIET_SECRET){
+    req.userdetails = {email:email};
+    next();
   }
+  else{
+    res.status(401).json("Invalid Access Secret");
+  }
+
+
+  // var {token} = req.headers;
+  // try {
+  //   admin.auth().verifyIdToken(token)
+  //     .then(function(decodedToken) {
+  //       req.userdetails = decodedToken;
+  //       next();
+  //     })
+  //     .catch(function(error) {
+  //       console.error("Error verifying ID token:", error);
+  //       res.status(401).json("Invalid Access Token May be Expired");
+  //     });
+  // } catch (error) {
+  //   console.error("Error verifying ID token:", error);
+  //   res.status(401).json("Invalid Access Token May be Expired");
+  // }
 };
 
 module.exports = decodeToken;
