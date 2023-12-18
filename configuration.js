b078ecb980bff8488763aee658eff19bf838d6ab
Santhosh <shanmugamsanthosh22@gmail.com>
2023-12-18 11:59:59 +0530

    today--

--- configuration.js
+++ configuration.js
@@ -1,14 +1,8 @@
 var admin = require('firebase-admin');
-// var credentials = require('./credentials.json');
-
-const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY;
+var credentials = require('./credentials.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert({
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: firebasePrivateKey.replace(/\n/g, '\n'),
-    })
+  credential: admin.credential.cert(credentials)
 });
 
 module.exports = admin;
